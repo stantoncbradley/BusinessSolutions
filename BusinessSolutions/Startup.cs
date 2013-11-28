e9b4287d42cf88f4ed6403e05462a980970da77c
Stanton Cole Bradley <stantoncbradley@gmail.com>
2013-11-28 11:19:38 -0600

    first commit
    shared layout
    index page
    control, model started

---
+++ BusinessSolutions/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BusinessSolutions.Startup))]
+namespace BusinessSolutions
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
